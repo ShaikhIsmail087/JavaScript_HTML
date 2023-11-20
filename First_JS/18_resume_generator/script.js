6e1b25213a54f40944e61ebfecb9d9a0bb12df4d
ShaikhIsmail087 <shaikhismail0876262@gmail.com>
2023-11-20 16:10:06 +0530

    Resume Generator Project-2

--- First_JS/18_resume_generator/script.js
+++ First_JS/18_resume_generator/script.js
@@ -16,7 +16,7 @@ function addNewWEField() {
 
 }
 
-function addNewAQField(){
+function addNewAQField() {
     let newNode = document.createElement('textarea');
     newNode.classList.add('form-control');
     newNode.classList.add('aqField');
@@ -28,4 +28,59 @@ function addNewAQField(){
     let aqAddButtonOb = document.getElementById('aqAddButton');
 
     aqOb.insertBefore(newNode, aqAddButtonOb);
+}
+
+//generate cv
+function generateCV() {
+    // console.log("generating cv..");
+    let nameField = document.getElementById("nameField").value;
+    let objectiveField = document.getElementById("objectiveField").value;
+
+    let nameT1 = document.getElementById("nameT1");
+
+    nameT1.innerHTML = nameField;
+
+    //direct
+    //name
+    document.getElementById("nameT2").innerHTML = nameField;
+    document.getElementById("objectiveT").innerHTML = objectiveField;
+    //contact
+    document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+    //address
+    document.getElementById("addressT").innerHTML = document.getElementById("addressField").value;
+    //links
+    document.getElementById("fbT").innerHTML = document.getElementById("fbField").value;
+
+    document.getElementById("instaT").innerHTML = document.getElementById("instaField").value;
+
+    document.getElementById("linkedT").innerHTML = document.getElementById("linkedField").value;
+
+    //we
+    let wes = document.getElementsByClassName("weField");
+    let str = "";
+    for (const e of wes) {
+        str = str + `<li> ${e.value}</li>`;
+    }
+
+    document.getElementById("weT").innerHTML = str;
+
+    //aq
+
+    let aqs = document.getElementsByClassName("aqField");
+    let str1 = '';
+    for (const e of aqs) {
+        str1 += `<li> ${e.value}</li>`
+    }
+
+    document.getElementById("aqT").innerHTML = str1;
+
+    document.getElementById("cv-form").style.display='none';
+    document.getElementById('cv-template').style.display='block'
+
+}
+
+//printing cv
+
+function printCV(){
+    window.print(); 
 }
