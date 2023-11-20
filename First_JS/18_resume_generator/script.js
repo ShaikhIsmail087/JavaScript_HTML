c128e620e25dbf09164ddd161e2b4dd245b04f88
ShaikhIsmail087 <shaikhismail0876262@gmail.com>
2023-11-20 14:13:51 +0530

    Resume Generator using Javascript-1

---
+++ First_JS/18_resume_generator/script.js
@@ -0,0 +1,31 @@
+
+function addNewWEField() {
+    // console.log("Adding new field..");
+
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('weField');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute('rows', 3);
+    newNode.setAttribute('placeholder', 'Enter here');
+
+    let weOb = document.getElementById('we');
+    let weAddButtonOb = document.getElementById('weAddButton');
+
+    weOb.insertBefore(newNode, weAddButtonOb);
+
+}
+
+function addNewAQField(){
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('aqField');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute('rows', 3);
+    newNode.setAttribute('placeholder', 'Enter here');
+
+    let aqOb = document.getElementById('aq');
+    let aqAddButtonOb = document.getElementById('aqAddButton');
+
+    aqOb.insertBefore(newNode, aqAddButtonOb);
+}
