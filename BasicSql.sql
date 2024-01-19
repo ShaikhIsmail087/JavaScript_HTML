d38da7d205d2fabf66f5369143d96f2e2c977269
ShaikhIsmail087 <shaikhismail0876262@gmail.com>
2024-01-19 15:58:53 +0530

    Basic SQL multiple tables

--- BasicSql.sql
+++ BasicSql.sql
@@ -62,3 +62,29 @@ select count(*) as customer_id,order_id as age from Orders group by item order b
 
 --Having clause
 select sum(age) as first_name, last_name as country from Customers group by last_name having age<100 order by age desc;
+
+
+select max(age) from Customers;
+select max(age) from Customers where age<(select max(age) from Customers);
+select max(age) from Customers where age<(select max(age) from Customers where age<(select max(age) from Customers));
+select max(age) from Customers where age<(select max(age) from Customers where age<(select max(age) from Customers where age<(select max(age) from Customers)));
+alter table Customers add seller varchar(20);
+alter table Customers drop seller;
+insert into Customers values(6,'Rashid','Ali',27,'IN','Nokia'),(7,'Afsal','khan',27,'SA','Apple'),(8,'Suraj','Shindhe',27,'BA','Redmi');
+update Customers set seller='flipkart' where customer_id >=3 and customer_id<=6;
+update Customers set seller='flipkart' where customer_id between 4 and 6;
+--Multiple Tables (Concept of Foreign key)
+create table university(university_id int primary key,university_name varchar(20));
+insert into university values(1,'MIT'),(2,'Harvard');
+create table student (student_id int primary key auto_increment,student_name varchar(30) not null,age tinyint not null,university_id int,foreign key (university) references university(university_id));
+insert into student values(1,'Kevin',19,null),(2,'Adam',22,1),(3,'Ana',23,2);
+drop table student;
+Updating dependent tables example
+update Customers set customer_id=10 where customer_id=1;
+--Error: FOREIGN KEY constraint failed
+drop table university;
+--error: cannot drop table 'uni...' refernced by foreign key constraint
+drop table student; --successfully deleted
+create table student(id int primary key ,student_name varchar(30),university int,constraint const foreign key(university) references university(university_id) on update cascade on delete cascade);
+insert into student values(1,'Kevin',null),(2,'Adam',1),(3,'Ana',2),(4,'Daniel',1);
+update Customers set customer_id=10 where customer_id=1;--updated succ.. due to cascade
